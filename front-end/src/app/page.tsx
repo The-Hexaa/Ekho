c37c04d3adc02584eda33a8ddcc63387538ea733
Nauman073 <nauman.tasawar@thehexaa.com>
2024-06-24 14:58:46 +0500

    initial commit

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,90 @@
+'use client';
+import {
+  LiveKitRoom,
+  RoomAudioRenderer,
+  useLocalParticipant,
+} from '@livekit/components-react';
+import { useState, useEffect } from "react";
+import '@fortawesome/fontawesome-free/css/all.css'; // Ensure Font Awesome is available
+import styles from './style/home.module.css'; // Import the CSS module
+
+export default function Home() {
+  const [token, setToken] = useState<string | null>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [callDuration, setCallDuration] = useState(0);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isConnected) {
+      timer = setInterval(() => {
+        setCallDuration(prevDuration => prevDuration + 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [isConnected]);
+
+  const handleConnect = async () => {
+    const { accessToken, url } = await fetch('/api/token').then(res => res.json());
+    setToken(accessToken);
+    setUrl(url);
+    setIsConnected(true);
+  };
+
+  const handleDisconnect = () => {
+    setToken(null);
+    setUrl(null);
+    setIsConnected(false);
+    setCallDuration(0);
+  };
+
+  return (
+    <>
+      <main className={styles.main}>
+        <i className={`fas fa-user-circle ${styles.userIcon}`}></i>
+        <div className={styles.buttonContainer}>
+          <button 
+            onClick={handleConnect}
+            className={styles.button}
+            disabled={isConnected}
+          >
+            <i className="fas fa-phone fa-2x"></i>
+          </button>
+          <button 
+            onClick={handleDisconnect}
+            className={styles.button}
+            disabled={!isConnected}
+          >
+            <i className="fas fa-phone-slash fa-2x"></i>
+          </button>
+        </div>
+        {isConnected && <div className={styles.callDuration}>Call Duration: {Math.floor(callDuration / 60)}:{callDuration % 60 < 10 ? '0' : ''}{callDuration % 60}</div>}
+        {token && url && (
+          <LiveKitRoom
+            token={token}
+            serverUrl={url}
+            connectOptions={{ autoSubscribe: true }}
+            onDisconnected={() => setIsConnected(false)}
+          >
+            <ActiveRoom />
+          </LiveKitRoom>
+        )}
+      </main>
+    </>
+  );
+}
+
+const ActiveRoom = () => {
+  const { localParticipant, isMicrophoneEnabled } = useLocalParticipant();
+  return (
+    <>
+      <RoomAudioRenderer />
+      <button onClick={() => {
+        localParticipant?.setMicrophoneEnabled(!isMicrophoneEnabled)
+      }}>
+        Toggle Microphone
+      </button>
+      <div>Audio Enabled: { isMicrophoneEnabled ? 'Unmuted' : 'Muted' }</div>
+    </>
+  );
+};
